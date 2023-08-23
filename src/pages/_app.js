b654fbe7006ffa3948f20386a82a8793663c8dca
William Lim <weinianlim26@gmail.com>
2023-08-22 22:21:18 -0700

    SSR

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>YourKpop</title>
+        <meta property="og:title" content="YourKpop" key="title" />
         <link rel="yourkpop icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,49 +1,8 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import Script from 'next/script'
 import Layout from '@/components/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export async function getStaticProps() {
-  const fields = ['id', 'date', 'title', 'slug', 'status', 'content.rendered']
-  const res = await fetch(
-    `${process.env.WORDPRESS_API_URL}/wp-json/wp/v2/posts?_fields=${fields.join(',')}`,
-    { cache: 'no-store' })
-  const posts = await res.json()
-
-  const data = posts.reduce((mappedPosts, post ) => {
-    const { id, date, slug, status, title, content } = post
-
-    const isPublished = status === 'publish'
-
-    const regex = /<img[^>]*>|<p[^>]*>.*?<\/p>/g
-    const matches = content.rendered.match(regex)
-
-    const modifiedContent = matches.map((p) => (
-      p.replace(/<\/?p>/g, '').replace(/target="_blank"/g, 'target="_blank" class="text-blue-500"')
-    ))
-
-    if (isPublished) {
-      mappedPosts.push({
-        id,
-        date,
-        slug,
-        title: title.rendered,
-        imgCover: modifiedContent.find(content => content.includes('img'))
-      })
-    }
-
-    return mappedPosts
-  }, [])
-
-  return {
-    props: {
-      posts: data
-    }
-  }
-}
 
 export default function Home({ posts }) {
   return (
@@ -78,4 +37,44 @@ export default function Home({ posts }) {
       </section>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const fields = ['id', 'date', 'title', 'slug', 'status', 'content.rendered']
+  const res = await fetch(
+    `${process.env.WORDPRESS_API_URL}/wp-json/wp/v2/posts?_fields=${fields.join(',')}`,
+    { cache: 'no-store' }
+  );
+  const posts = await res.json();
+
+  const data = posts.reduce((mappedPosts, post) => {
+    const { id, date, slug, status, title, content } = post;
+
+    const isPublished = status === 'publish';
+
+    const regex = /<img[^>]*>|<p[^>]*>.*?<\/p>/g;
+    const matches = content.rendered.match(regex);
+
+    const modifiedContent = matches.map((p) =>
+      p.replace(/<\/?p>/g, '').replace(/target="_blank"/g, 'target="_blank" class="text-blue-500"')
+    );
+
+    if (isPublished) {
+      mappedPosts.push({
+        id,
+        date,
+        slug,
+        title: title.rendered,
+        imgCover: modifiedContent.find((content) => content.includes('img')),
+      });
+    }
+
+    return mappedPosts;
+  }, []);
+
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
